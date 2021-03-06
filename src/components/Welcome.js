2570b18b2b9058e714845cf2d5f8cc4235511c43
digbijay1 <digbijay.das@suiit.ac.in>
2021-03-06 18:11:45 +0530

    second commit

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,17 @@
+import React from 'react'
+
+function Welcome(props) {
+    return (
+        <div>
+            <h1>
+                Hey !{props.name}
+                </h1>
+                <h2>
+                    Welcome to Newton School
+                </h2>
+        </div>
+    )
+}
+
+export default Welcome
 // write code for Welcome component here
